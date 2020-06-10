e8827fa4deda21265d8b848a124d7b6887e8fa89
fedina562 <rln562@163.com>
2020-06-10 16:30:39 +0800

    注释掉pom引入的webflux，引入web，才可使用模板

--- src/main/java/com/example/fristdemo/config/RouterFunctionConfig.java
+++ src/main/java/com/example/fristdemo/config/RouterFunctionConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.example.fristdemo.config;
 
 import com.example.fristdemo.doamin.User;
@@ -11,10 +12,12 @@ import reactor.core.publisher.Mono;
 
 import java.util.Collection;
 
+*/
 /**
  * 路由函数,等同于Controller中的mapping方式\
  * java版的se文件
- */
+ *//*
+
 //表示配置对象，逐一替代
 @Configuration
 public class RouterFunctionConfig {
@@ -22,13 +25,15 @@ public class RouterFunctionConfig {
     @Autowired
     private UserRepository userRepository;
 
-    /**
+    */
+/**
      * 定义get请求，返回所有的用户对象 URI为/person/find/all
      * Flux 是0-N个对象集合
      * mono 是0-1个对象集合，单一对象 类似于java8的Optional
      * Reactive 中的Flux与Mono 是异步处理（非阻塞）
      * Flux 或者 Mono都是Publisher
-     */
+     *//*
+
     @Bean
     public RouterFunction<ServerResponse> personFindAll(){
         return RouterFunctions.route(RequestPredicates.GET("/person/find/all"),
@@ -41,3 +46,4 @@ public class RouterFunctionConfig {
 
     }
 }
+*/
