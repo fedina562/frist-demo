5df654ec5d9f3ee315f843ac6a71226d818b93e3
fedina562 <rln562@163.com>
2020-06-11 15:53:13 +0800

    整合mapper

---
+++ src/main/java/com/example/fristdemo/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.example.fristdemo.mapper;
+
+import com.example.fristdemo.pojo.SysUser;
+import com.example.fristdemo.utils.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
