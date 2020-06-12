cb28768cbb11212ffe8923507a9bd4d496933d07
fedina562 <rln562@163.com>
2020-06-12 13:38:43 +0800

    实现mybatis的CURD功能

--- src/main/java/com/example/fristdemo/mapper/SysUserMapper.java
+++ src/main/java/com/example/fristdemo/mapper/SysUserMapper.java
@@ -3,5 +3,9 @@ package com.example.fristdemo.mapper;
 import com.example.fristdemo.pojo.SysUser;
 import com.example.fristdemo.utils.MyMapper;
 
+import java.util.List;
+
 public interface SysUserMapper extends MyMapper<SysUser> {
+
+    List<SysUser> queryUserSimplyInfoById(String id);
 }
---
+++ src/main/java/com/example/fristdemo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.fristdemo.service;
+
+
+import com.example.fristdemo.pojo.SysUser;
+
+import java.util.List;
+
+public interface UserService {
+
+	public void saveUser(SysUser user) throws Exception;
+
+	public void updateUser(SysUser user);
+
+	public void deleteUser(String userId);
+
+	public SysUser queryUserById(String userId);
+
+	public List<SysUser> queryUserList(SysUser user);
+
+	public List<SysUser> queryUserListPaged(SysUser user, Integer page, Integer pageSize);
+
+	public SysUser queryUserByIdCustom(String userId);
+	
+	public void saveUserTransactional(SysUser user);
+}
