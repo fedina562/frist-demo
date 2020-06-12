dd3e80d79a7243dac71aff7dec960861f57b7a2a
fedina562 <rln562@163.com>
2020-06-12 15:16:21 +0800

    整合异步任务

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # frist-demo
 SpringBoot开发常用技术整合
+
+全局异常处理：
+@ControllerAdvice @ExceptionHandler(value = Exception.class)
+
+开启定时任务：
+FristDemoApplication使用注解@EnableScheduling开启定时任务，会自动扫描
+定义@Scheduled作为组件被容器扫描执行（在线cron表达式生成器 https://cron.qqe2.com/）
+
+开启异步任务：
+FristDemoApplication使用注解@EnableAsync来开启异步，会自动扫描
+定义@Component @Async作为组件被容器扫描执行
--- src/main/java/com/example/fristdemo/FristDemoApplication.java
+++ src/main/java/com/example/fristdemo/FristDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.fristdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -13,6 +14,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages= {"com.example.fristdemo", "org.n3r.idworker"})
 //开启定时任务
 @EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class FristDemoApplication {
 
 	public static void main(String[] args) {
